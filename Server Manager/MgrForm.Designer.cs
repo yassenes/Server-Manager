dd109a3d36396ed49b0141fe531dc540f8dda6ed
Yassen <yassenexc@gmail.com>
2024-05-13 11:33:25 +0300

    Add project files.

---
+++ Server Manager/MgrForm.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Server_Manager
+{
+    partial class MgrForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MgrForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.appList = new System.Windows.Forms.CheckedListBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.restartButton = new System.Windows.Forms.Button();
+            this.removeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("menuStrip1.BackgroundImage")));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(362, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.BackColor = System.Drawing.Color.Transparent;
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem,
+            this.exitToolStripMenuItem,
+            this.removeToolStripMenuItem});
+            this.fileToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.fileToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("fileToolStripMenuItem.Image")));
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.BackColor = System.Drawing.Color.Transparent;
+            this.addToolStripMenuItem.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("addToolStripMenuItem.BackgroundImage")));
+            this.addToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.addToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("addToolStripMenuItem.Image")));
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addToolStripMenuItem.Text = "Add";
+            this.addToolStripMenuItem.Click += new System.EventHandler(this.addToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.BackColor = System.Drawing.Color.Transparent;
+            this.exitToolStripMenuItem.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("exitToolStripMenuItem.BackgroundImage")));
+            this.exitToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.exitToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("exitToolStripMenuItem.Image")));
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Remove";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.BackColor = System.Drawing.Color.Transparent;
+            this.aboutToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.aboutToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("aboutToolStripMenuItem.Image")));
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // appList
+            // 
+            this.appList.BackColor = System.Drawing.Color.White;
+            this.appList.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.appList.FormattingEnabled = true;
+            this.appList.Location = new System.Drawing.Point(12, 27);
+            this.appList.Name = "appList";
+            this.appList.Size = new System.Drawing.Size(339, 210);
+            this.appList.TabIndex = 1;
+            this.appList.SelectedIndexChanged += new System.EventHandler(this.appList_SelectedIndexChanged);
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.Transparent;
+            this.startButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("startButton.BackgroundImage")));
+            this.startButton.FlatAppearance.BorderSize = 0;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.startButton.Location = new System.Drawing.Point(12, 246);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(108, 44);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.BackColor = System.Drawing.Color.Transparent;
+            this.closeButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("closeButton.BackgroundImage")));
+            this.closeButton.FlatAppearance.BorderSize = 0;
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.closeButton.Location = new System.Drawing.Point(243, 246);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(108, 44);
+            this.closeButton.TabIndex = 5;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = false;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // restartButton
+            // 
+            this.restartButton.BackColor = System.Drawing.Color.Transparent;
+            this.restartButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("restartButton.BackgroundImage")));
+            this.restartButton.FlatAppearance.BorderSize = 0;
+            this.restartButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.restartButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.restartButton.Location = new System.Drawing.Point(129, 246);
+            this.restartButton.Name = "restartButton";
+            this.restartButton.Size = new System.Drawing.Size(108, 44);
+            this.restartButton.TabIndex = 6;
+            this.restartButton.Text = "Restart";
+            this.restartButton.UseVisualStyleBackColor = false;
+            this.restartButton.Click += new System.EventHandler(this.restartButton_Click);
+            // 
+            // removeToolStripMenuItem
+            // 
+            this.removeToolStripMenuItem.BackColor = System.Drawing.Color.Transparent;
+            this.removeToolStripMenuItem.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("removeToolStripMenuItem.BackgroundImage")));
+            this.removeToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.removeToolStripMenuItem.Name = "removeToolStripMenuItem";
+            this.removeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.removeToolStripMenuItem.Text = "Exit";
+            this.removeToolStripMenuItem.Click += new System.EventHandler(this.removeToolStripMenuItem_Click);
+            // 
+            // MgrForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(362, 301);
+            this.Controls.Add(this.restartButton);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.appList);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MgrForm";
+            this.Text = "ServerMgr";
+            this.Load += new System.EventHandler(this.MgrForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.CheckedListBox appList;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button restartButton;
+        private System.Windows.Forms.ToolStripMenuItem removeToolStripMenuItem;
+    }
+}
+
---
+++ Server Manager/MgrForm.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server_Manager
+{
+    public struct ProcessInfo
+    {
+        public string FileName;
+        public Process FileProcess;
+    }
+
+    public partial class MgrForm : System.Windows.Forms.Form
+    {
+        public static Dictionary<string, ProcessInfo> dProcessList;
+
+        public MgrForm()
+        {
+            InitializeComponent();
+            dProcessList = new Dictionary<string, ProcessInfo>();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (appList.CheckedItems.Count > 0)
+            {
+                foreach (var i in appList.CheckedItems)
+                {
+                    appList.Items.Remove(i);
+                }
+            }
+            else
+            {
+                MessageBox.Show("There are no executables in the list.");
+            }
+        }
+
+        private void MgrForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Created by Ace");
+        }
+
+        private void addToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog()
+            {
+                Filter = "exe files|*.exe",
+                Multiselect = true
+            };
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                foreach (var f in ofd.FileNames)
+                {
+                    ProcessInfo processInfo = new ProcessInfo()
+                    {
+                        FileName = f,
+                        FileProcess = null
+                    };
+                    dProcessList.Add(Path.GetFileName(f), processInfo);
+                    appList.Items.Add(Path.GetFileName(f));
+                }
+            }
+        }
+
+        private void appList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void appCount_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (appList.CheckedItems.Count > 0)
+            {
+                foreach (var i in appList.CheckedItems)
+                {
+                    ProcessInfo processInfo = dProcessList[i.ToString()];
+
+                    if (processInfo.FileProcess != null)
+                    {
+                        if (!processInfo.FileProcess.HasExited)
+                            return;
+                    }
+
+                    ProcessStartInfo start = new ProcessStartInfo()
+                    {
+                        FileName = processInfo.FileName,
+                    };
+
+                    processInfo.FileProcess = new Process();
+                    processInfo.FileProcess = Process.Start(start);
+
+                    dProcessList[i.ToString()] = processInfo;
+                }
+            }
+            else
+            {
+                MessageBox.Show("There are no executables in the list.");
+            }
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            if (appList.CheckedItems.Count > 0)
+            {
+                foreach (var i in appList.CheckedItems)
+                {
+                    try
+                    {
+                        ProcessInfo processInfo = dProcessList[i.ToString()];
+
+                        if (processInfo.FileProcess != null)
+                        {
+                            processInfo.FileProcess.Kill();
+                            processInfo.FileProcess = null;
+
+                            dProcessList[i.ToString()] = processInfo;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("There are no executables in the list.");
+            }
+        }
+
+        private void restartButton_Click(object sender, EventArgs e)
+        {
+            if (appList.CheckedItems.Count > 0)
+            {
+                foreach (var i in appList.CheckedItems)
+                {
+                    try
+                    {
+                        ProcessInfo processInfo = dProcessList[i.ToString()];
+
+                        if (processInfo.FileProcess != null)
+                        {
+                            processInfo.FileProcess.Kill();
+
+                            Cursor.Current = Cursors.WaitCursor;
+
+                            Thread.Sleep(3000);
+
+                            ProcessStartInfo start = new ProcessStartInfo()
+                            {
+                                FileName = processInfo.FileName,
+                            };
+
+                            processInfo.FileProcess = new Process();
+                            processInfo.FileProcess = Process.Start(start);
+
+                            Cursor.Current = Cursors.Default;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("There are no executables in the list.");
+            }
+
+        }
+
+        private void removeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Do you really wish to exit this program?", "Exit confirmation", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void appCountText_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
